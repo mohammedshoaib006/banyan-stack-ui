6ba198c956e43c8bb9508e5c733adb8b4026efb0
Mohammed Shoaib <mohammedshoaib006@gmail.com>
2026-01-08 10:54:45 +0530

    intergated TailwindCss and Material Framework

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
